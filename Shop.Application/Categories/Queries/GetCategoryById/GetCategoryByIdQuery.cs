0f7066d99bbc0234f505a907f7ace4151045abc0
Alexey Samitov <samitov.alexey@gmail.com>
2025-05-01 20:33:31 +0200

    implement GetCategoryById command

---
+++ Shop.Application/Categories/Queries/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Shop.Application.Entities;
+
+namespace Shop.Application.Categories.Queries.GetCategoryById;
+public class GetCategoryByIdQuery: IRequest<CategoryVm>
+{
+    public int Id { get; set; }
+}
---
+++ Shop.Application/Categories/Queries/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Shop.Application.Entities;
+using Shop.Domain.Contracts;
+
+namespace Shop.Application.Categories.Queries.GetCategoryById;
+public class GetCategoryByIdQueryHandler(ICategoryRepository repository, IMapper mapper) 
+    : IRequestHandler<GetCategoryByIdQuery, CategoryVm>
+{
+    public async Task<CategoryVm> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+    {
+        var category = await repository.GetByIdAsync(request.Id);
+
+        if (category == null)
+            throw new NullReferenceException();
+
+        return mapper.Map<CategoryVm>(category);
+    }
+}
