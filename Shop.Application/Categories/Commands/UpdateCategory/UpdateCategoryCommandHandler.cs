c127d87fd0dd90969479a527de73e4a0667660d2
Alexey Samitov <samitov.alexey@gmail.com>
2025-05-02 08:45:24 +0200

    add category update command

---
+++ Shop.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Shop.Domain.Contracts;
+using Shop.Domain.Entities;
+
+namespace Shop.Application.Categories.Commands.UpdateCategory;
+public class UpdateCategoryCommandHandler(ICategoryRepository repository, IMapper mapper)
+    : IRequestHandler<UpdateCategoryCommand, int>
+{
+    public async Task<int> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Category
+        {
+            Id = request.Id,
+            Name = request.CategoryName,
+            Description = request.Description,
+            Products = request.Products
+        };
+
+        var result  = await repository.UpdateAsync(request.Id, category);
+        return result;
+    }
+}
