d3570aa3d2ff4cf89c960aa4470843cb7b16fb9a
Alexey Samitov <samitov.alexey@gmail.com>
2025-05-02 08:34:25 +0200

    add category create command

---
+++ Shop.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Shop.Application.Entities;
+using Shop.Domain.Entities;
+
+namespace Shop.Application.Categories.Commands.CreateCategory;
+public class CreateCategoryCommand: IRequest<CategoryVm>
+{
+    public string CategoryName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<Product> Products { get; set; }
+}
---
+++ Shop.Application/Categories/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Shop.Application.Entities;
+using Shop.Domain.Contracts;
+using Shop.Domain.Entities;
+
+namespace Shop.Application.Categories.Commands.CreateCategory;
+public class CreateCategoryCommandHandler(ICategoryRepository repository, IMapper mapper)
+    : IRequestHandler<CreateCategoryCommand, CategoryVm>
+{
+    public async Task<CategoryVm> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Category
+        {
+            Name = request.CategoryName,
+            Description = request.Description,
+            Products = request.Products
+        };
+
+        await repository.CreateAsync(category);
+        return mapper.Map<CategoryVm>(category);
+    }
+}
